2fa776999d7a7336ddab1833e1dbcb8544b61ac4
zFlxw <flxw@venterion.com>
2021-07-31 16:20:27 +0200

    Designed Login page, changed default routes

--- src/main/java/com/github/zflxw/loginjam/LoginScreen.java
+++ src/main/java/com/github/zflxw/loginjam/LoginScreen.java
@@ -1,9 +1,9 @@
 package com.github.zflxw.loginjam;
 
 import com.github.zflxw.loginjam.utils.Reader;
+import com.vtence.molecule.Request;
 import com.vtence.molecule.Response;
 import com.vtence.molecule.WebServer;
-import com.vtence.molecule.http.Cookie;
 import com.vtence.molecule.lib.CookieJar;
 import com.vtence.molecule.middlewares.Cookies;
 import com.vtence.molecule.routing.Routes;
@@ -37,25 +37,17 @@ public class LoginScreen {
         }))
         .add(new Cookies())
             .route(new Routes() {{
-                map("/").to(request -> Response.ok().body(Reader.readResource("index.html")).done());
+                map("/").to(request -> (isLoggedIn(request) ? Response.redirect("/dashboard").done() : Response.redirect("/login")).done());
 
                 get("/login").to(request -> Response.ok().body(Reader.readResource("login.html")).done());
                 get("/register").to(request -> Response.ok().body(Reader.readResource("register.html")).done());
-                get("/unauthorized").to(request -> Response.ok().body(Reader.readResource("unauthorized.html")).done());
 
                 get("/dashboard").to(request -> {
-                    CookieJar cookies = CookieJar.get(request);
-
-                    if (cookies.has("token")) {
-                        Cookie cookie = cookies.get("token");
-                        String token = cookie.value();
-
-                        if (tokens.containsKey(token)) {
-                            return Response.ok().body(Reader.readResource("dashboard.html")).done();
-                        }
+                    if (isLoggedIn(request)) {
+                        return Response.ok().body(Reader.readResource("dashboard.html")).done();
                     }
 
-                    return Response.redirect("/unauthorized").done();
+                    return Response.ok().body(Reader.readResource("unauthorized.html")).done();
                 });
 
                 post("/register").to(request -> {
@@ -65,15 +57,15 @@ public class LoginScreen {
                         String passwordRepeat = request.part("password_repeat").value();
 
                         if (email.isBlank() || password.isBlank() || passwordRepeat.isBlank()) {
-                            return Response.redirect("/register?error_code=0").done();
+                            return Response.redirect("/register?error-code=0").done();
                         }
 
                         if (!password.equals(passwordRepeat)) {
-                            return Response.redirect("/register?error_code=1").done();
+                            return Response.redirect("/register?error-code=1").done();
                         }
 
                         if (data.containsKey(email.toLowerCase())) {
-                            return Response.redirect("/register?error_code=2").done();
+                            return Response.redirect("/register?error-code=2").done();
                         }
 
                         data.put(email, String.valueOf(hash(password)));
@@ -89,7 +81,7 @@ public class LoginScreen {
                         exception.printStackTrace();
                     }
 
-                    return Response.redirect("/").done();
+                    return Response.redirect("/dashboard").done();
                 });
 
                 post("/login").to(request -> {
@@ -100,15 +92,15 @@ public class LoginScreen {
                         CookieJar cookies = CookieJar.get(request);
 
                         if (cookies.has("token") && tokens.containsKey(cookies.get("token").value())) {
-                            return Response.redirect("/login?error_code=2").done();
+                            return Response.redirect("/login?error-code=2").done();
                         }
 
                         if (email.isBlank() || password.isBlank()) {
-                            return Response.redirect("/login?error_code=0").done();
+                            return Response.redirect("/login?error-code=0").done();
                         }
 
                         if (!String.valueOf(hash(password)).equals(data.getProperty(email.toLowerCase()))) {
-                            return Response.redirect("/login?error_code=1").done();
+                            return Response.redirect("/login?error-code=1").done();
                         }
 
                         UUID token = UUID.randomUUID();
@@ -118,7 +110,7 @@ public class LoginScreen {
                         exception.printStackTrace();
                     }
 
-                    return Response.redirect("/").done();
+                    return Response.redirect("/dashboard").done();
                 });
 
                 post("/logout").to(request -> {
@@ -143,5 +135,11 @@ public class LoginScreen {
      */
     private static int hash(String password) {
         return password.hashCode() + 10 + password.charAt(0);
+    }
+
+    private static boolean isLoggedIn(Request request) {
+        CookieJar cookies = CookieJar.get(request);
+
+        return cookies.has("token") && tokens.containsKey(cookies.get("token").value());
     }
 }
